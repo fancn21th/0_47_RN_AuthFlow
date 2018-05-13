618f29be0d4a79647bc822726598dd764623d48a
fancn <fanyj@yikeer.com>
2018-05-13 23:14:20 +0800

    redirection action

--- App/Auth/withLogined.js
+++ App/Auth/withLogined.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getLogined } from '../Reducers'
 import { withNavigation } from 'react-navigation'
+import { REDIRECTION } from '../Actions/Types/Navigation'
 
 const withLoginedContainer = Component => (props) => {
-  const { isLogined, navigation } = props
+  const { isLogined, navigation, onRedirect } = props
   // if user has not logined then redirect to Auth screen
   if(!isLogined) {
+    onRedirect(navigation.state.routeName)
     navigation.navigate('Auth')
   }
   return <Component
@@ -18,11 +20,22 @@ const mapStateToProps = (state) => ({
   isLogined: getLogined(state)
 })
 
+const mapDispatchToProps = (dispatch) => ({
+  onRedirect(screen) {
+    dispatch({
+      type: REDIRECTION,
+      payload: {
+        screen
+      }
+    })
+  }
+})
+
 const withLogined = Component => (
   withNavigation(
     connect(
       mapStateToProps,
-      null
+      mapDispatchToProps
     )(withLoginedContainer(Component))
   )
 )
